5863a13fd5c00c2342e5d6021f6157544f9d7c13
sonic6design <sonic6design@gmail.com>
2023-06-27 11:12:57 -0500

    Initial Commit

---
+++ MetricRhythmApp/MetricRhythmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricRhythmAppApp.swift
+//  MetricRhythmApp
+//
+//  Created by Matthew Novachis on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MetricRhythmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
